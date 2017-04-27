360e9f4eb3a4ea2f5eae2d2729f18a1c9a390a98
ckorkos <chkorkos@gmail.com>
2017-04-27 16:54:22 -0700

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -260,7 +260,7 @@ function changeDivContent(stuff) {
       var mq = window.matchMedia("(min-width: 780px)");
       if(mq.matches) {
         // window width is at least 780 px
-        $("#loaded").css("height", "2300px");
+        $("#loaded").css("height", "2000px");
       }
       else {
         // window width is less than 780 px
